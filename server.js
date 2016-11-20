f896d9646d4eca18427d405daffcc2ca5dd84d93
kiranhsgithub <hskiran.bms@gmail.com>
2016-11-20 16:15:48 -0500

    get message api

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-var mongo = require('mongodb').MongoClient;
+var mongoose = require('mongoose');
 
-var database;
+var Message = mongoose.model('Message', {
+    msg: String
+});
 
 app.use(bodyParser.json());
 
@@ -13,18 +15,28 @@ app.use(function(req, res, next){
     next();
 });
 
+app.get('/api/message', GetMessages);
+
 app.post('/api/message', function(req, res){
     console.log(req.body);
-    database.collection('messages').insertOne(req.body);
+    var message = new Message(req.body);
+
+    message.save();
+
     res.status(200);
 
 });
 
-mongo.connect("mongodb://localhost:27017/test", function(err,db){
+function GetMessages(req,res)
+{
+    Message.find({}).exec(function (err, result) {
+        res.send(result);
+    });
+}
+
+mongoose.connect("mongodb://localhost:27017/test", function(err,db){
     if(!err){
         console.log("We are connected to Mongo");
-        database = db;
-        db.collection('messages').insertOne({'msg':'test'});
     }
 });
 
